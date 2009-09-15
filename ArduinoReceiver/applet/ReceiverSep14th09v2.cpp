cd36f52414ac7617ae419cea6b89615dac33cff8
Ian Hern <ianhern@gmail.com>
2009-09-15 00:11:16 -0600

    InitialCommit of Arduino Code.

---
+++ ArduinoReceiver/applet/ReceiverSep14th09v2.cpp
@@ -0,0 +1,135 @@
+/**
+ * An Mirf example which copies back the data it recives.
+ *
+ * Pins:
+ * Hardware SPI:
+ * MISO -> 12
+ * MOSI -> 11
+ * SCK -> 13
+ *
+ * Configurable:
+ * CE -> 8
+ * CSN -> 7
+ *
+ */
+
+#include <Spi.h>
+#include <mirf.h>
+#include <nRF24L01.h>
+
+#define messageLength 3
+
+#define rudderServo '0'
+
+#define minServoPosition 900
+#define maxServoPosition 4700
+
+#include "WProgram.h"
+void setup();
+void loop();
+void setServoPosition(byte high, byte low);
+void setServoParameters();
+void setServoSpeed();
+void setup(){
+  
+  setServoParameters();
+  setServoSpeed();
+  
+  Serial.begin(9600);
+  
+  /*
+   * Setup pins / SPI.
+   */
+   
+  Mirf.init();
+  
+  /*
+   * Configure reciving address.
+   */
+   
+  Mirf.setRADDR((byte *)"bRecv");
+  
+  /*
+   * Set the payload length to sizeof(unsigned long) the
+   * return type of millis().
+   *
+   * NB: payload on client and server must be the same.
+   */
+   
+  Mirf.payload = messageLength; // sizeof(unsigned long);
+  
+  /*
+   * Write channel and payload config then power up reciver.
+   */
+   
+  Mirf.config();
+  
+  //Serial.println("Listening..."); 
+}
+
+void loop(){
+  /*
+   * A buffer to store the data.
+   */
+   
+  byte data[Mirf.payload];
+  
+  /*
+   * If a packet has been recived.
+   */
+  if(Mirf.dataReady()){
+    
+    do{
+      Mirf.getData(data);
+      
+     switch (data[0])
+     {
+       case rudderServo:
+         setServoPosition(data[1], data[2]);
+         break;
+     }    
+      
+    }while(!Mirf.rxFifoEmpty());
+  }
+}
+
+void setServoPosition(byte high, byte low)
+{
+  Serial.print(0x80,BYTE);    //start byte
+  Serial.print(0x01,BYTE);    //device id
+  Serial.print(0x04,BYTE);    //command number
+  Serial.print(rudderServo,BYTE);  //servo number
+  Serial.print(high, BYTE);
+  Serial.print(low, BYTE);
+}
+
+void setServoParameters()
+{
+  Serial.print(0x80,BYTE);    //start byte
+  Serial.print(0x01,BYTE);    //device id
+  Serial.print(0x00,BYTE);    //command number
+  Serial.print(rudderServo,BYTE);  //servo number
+  Serial.print(0x4F,BYTE);  // set direction to forward, servo to on, and range to 15
+}
+
+void setServoSpeed()
+{
+  Serial.print(0x80,BYTE);    //start byte
+  Serial.print(0x01,BYTE);    //device id
+  Serial.print(0x01,BYTE);    //command number
+  Serial.print(rudderServo,BYTE);  //servo number
+  Serial.print(0x00,BYTE);  // 0 is default speed (instantainious)
+}
+
+int main(void)
+{
+	init();
+
+	setup();
+    
+	for (;;)
+		loop();
+        
+	return 0;
+}
+
---
+++ ArduinoServer/servoSep14st09v3.pde
@@ -0,0 +1,69 @@
+byte rudderServo = 0x00;
+
+// in half micro seconds
+int minServoPosition = 900;
+int maxServoPosition = 4700;
+
+void setup()
+{
+  Serial.begin(9600);
+  
+  setServoParameters();
+  setServoSpeed();
+  
+  
+}
+
+void loop()
+{
+  setServoPosition(900);
+  delay(2000);
+  setServoPosition(4700);
+  delay(2000);
+}
+
+
+void setServoPosition(int newPosition)
+{
+  // we restrict the range to 450 - 2350 micro seconds so the servo dosen't break
+  if(newPosition < minServoPosition)
+  {
+    newPosition = minServoPosition;
+  }
+  else if(newPosition > maxServoPosition)
+  {
+    newPosition = maxServoPosition;
+  }
+  
+  byte lowByte, highByte;
+  
+  unsigned int temp;
+  temp = newPosition & 0x1f80;  //get bits 8 thru 13 of position
+  highByte = temp >> 7;     //shift bits 8 thru 13 by 7
+  lowByte = newPosition & 0x7f; //get lower 7 bits of position
+  
+  Serial.print(0x80,BYTE);    //start byte
+  Serial.print(0x01,BYTE);    //device id
+  Serial.print(0x04,BYTE);    //command number
+  Serial.print(rudderServo,BYTE);  //servo number
+  Serial.print(highByte, BYTE);
+  Serial.print(lowByte, BYTE);
+}
+
+void setServoParameters()
+{
+  Serial.print(0x80,BYTE);    //start byte
+  Serial.print(0x01,BYTE);    //device id
+  Serial.print(0x00,BYTE);    //command number
+  Serial.print(rudderServo,BYTE);  //servo number
+  Serial.print(0x4F,BYTE);  // set direction to forward, servo to on, and range to 15
+}
+
+void setServoSpeed()
+{
+  Serial.print(0x80,BYTE);    //start byte
+  Serial.print(0x01,BYTE);    //device id
+  Serial.print(0x01,BYTE);    //command number
+  Serial.print(rudderServo,BYTE);  //servo number
+  Serial.print(0x00,BYTE);  // 0 is default speed (instantainious)
+}
